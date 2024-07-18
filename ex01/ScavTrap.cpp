136438b763daa1385dccda89d12fc666e8c4cdd0
jpaul-kr <jonpk555@gmail.com>
2024-07-18 19:30:00 +0200

    starting ex01

---
+++ ex01/ScavTrap.cpp
@@ -0,0 +1,5 @@
+#include "ScavTrap.hpp"
+
+int	main()
+{
+}
---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,13 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+# include <iostream>
+# include <string>
+
+using std::string;
+
+class ScavTrap
+{
+};
+
+
+#endif
